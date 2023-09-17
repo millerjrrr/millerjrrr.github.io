8e51e08a2192d8e83fbdf16eb559cf852f7093b8
millerjrrr <millerjr@tcd.ie>
2023-09-17 14:04:21 -0300

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/jacob-and-maiza.jpg") {
+    myImage.setAttribute("src", "images/j-m-star.jpg");
+  } else {
+    myImage.setAttribute("src", "images/jacob-and-maiza.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Maiza is still cool, ${storedName}`;
+}
+
+
+myButton.onclick = () => {
+  setUserName();
+};
